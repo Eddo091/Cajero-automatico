330f2522fc7e3c83aea4256983aa4fa3c12a8886
Kathia Berrios <kathiamarce17@gmail.com>
2020-02-05 14:23:54 -0600

    Cambios

--- Cajero automatico/Form1.Designer.cs
+++ Cajero automatico/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.lblTotal = new System.Windows.Forms.Label();
             this.txtCantidad = new System.Windows.Forms.TextBox();
             this.btnRetirar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRetirar2 = new System.Windows.Forms.Button();
+            this.txtcCant = new System.Windows.Forms.TextBox();
+            this.lblTotal2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblCantidad
@@ -64,7 +68,7 @@
             // btnRetirar
             // 
             this.btnRetirar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRetirar.Location = new System.Drawing.Point(104, 172);
+            this.btnRetirar.Location = new System.Drawing.Point(428, 9);
             this.btnRetirar.Name = "btnRetirar";
             this.btnRetirar.Size = new System.Drawing.Size(98, 35);
             this.btnRetirar.TabIndex = 3;
@@ -72,11 +76,53 @@
             this.btnRetirar.UseVisualStyleBackColor = true;
             this.btnRetirar.Click += new System.EventHandler(this.btnRetirar_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cantidad a pagar";
+            // 
+            // btnRetirar2
+            // 
+            this.btnRetirar2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRetirar2.Location = new System.Drawing.Point(428, 53);
+            this.btnRetirar2.Name = "btnRetirar2";
+            this.btnRetirar2.Size = new System.Drawing.Size(98, 35);
+            this.btnRetirar2.TabIndex = 5;
+            this.btnRetirar2.Text = "Retirar";
+            this.btnRetirar2.UseVisualStyleBackColor = true;
+            this.btnRetirar2.Click += new System.EventHandler(this.btnRetirar2_Click);
+            // 
+            // txtcCant
+            // 
+            this.txtcCant.Location = new System.Drawing.Point(167, 58);
+            this.txtcCant.Name = "txtcCant";
+            this.txtcCant.Size = new System.Drawing.Size(100, 20);
+            this.txtcCant.TabIndex = 6;
+            // 
+            // lblTotal2
+            // 
+            this.lblTotal2.AutoSize = true;
+            this.lblTotal2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal2.Location = new System.Drawing.Point(301, 62);
+            this.lblTotal2.Name = "lblTotal2";
+            this.lblTotal2.Size = new System.Drawing.Size(61, 20);
+            this.lblTotal2.TabIndex = 7;
+            this.lblTotal2.Text = "Total: ?";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 233);
+            this.ClientSize = new System.Drawing.Size(597, 233);
+            this.Controls.Add(this.lblTotal2);
+            this.Controls.Add(this.txtcCant);
+            this.Controls.Add(this.btnRetirar2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnRetirar);
             this.Controls.Add(this.txtCantidad);
             this.Controls.Add(this.lblTotal);
@@ -98,6 +144,10 @@
         private System.Windows.Forms.Label lblTotal;
         private System.Windows.Forms.TextBox txtCantidad;
         private System.Windows.Forms.Button btnRetirar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRetirar2;
+        private System.Windows.Forms.TextBox txtcCant;
+        private System.Windows.Forms.Label lblTotal2;
     }
 }
 
--- Cajero automatico/Form1.cs
+++ Cajero automatico/Form1.cs
@@ -31,7 +31,8 @@ namespace Cajero_automatico
 
                 
                 }
-                if(n>0)
+
+               if(n>0)
                 {
                     respuesta += n + (denominacion > 1 ? " Billetes de " : " Monedas de ") + denominacion + "\n" ;
                 
@@ -41,6 +42,34 @@ namespace Cajero_automatico
             }
 
             lblTotal.Text = respuesta;
+        }
+
+        private void btnRetirar2_Click(object sender, EventArgs e)
+        {
+            double[] denominaciones = { 100, 50, 20, 10, 5, 1, 0.5, 0.25, 0.1, 0.05, 0.01 };
+           double cantidad = double.Parse(txtcCant.Text);
+            sbyte n = 0;
+            string respuesta = "Total: \n";
+            foreach (double denominacion in denominaciones)
+            {
+                while (denominacion <= Math.Round(cantidad, 2))
+                {
+                    n++;
+                    cantidad -= denominacion;
+
+
+                }
+
+                if (n < 0)
+                {
+                    respuesta += n + (denominacion > 1 ? " Billetes de " : " Monedas de ") + denominacion + "\n";
+
+                }
+                n = 0;
+
+            }
+           
+            lblTotal2.Text = respuesta ;
         }
     }
 }
